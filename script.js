162774cb388dba373e917aece9b3b6024f6fc4da
afu6656gu-ux <afu6656gu@gmail.com>
2025-09-09 22:23:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.getElementById("moreBtn");
+    btn.addEventListener("click", function() {
+        alert("الأهلي نادي القرن - مزيد من التفاصيل قريبًا!");
+    });
+});
